7c5e220b91f2fdbfd02d47049e72a2e03141e7ad
apthos <david00lara@gmail.com>
2020-07-22 12:39:52 -0700

    Added events table view to events view controller

--- Event Times/View Controllers/EventsViewController.m
+++ Event Times/View Controllers/EventsViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "EventsViewController.h"
+#import "EventCell.h"
+#import "Event.h"
 
-@interface EventsViewController ()
+@interface EventsViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) NSMutableArray *events;
+
+@property (strong, nonatomic) IBOutlet UITableView *eventsTableView;
 
 @end
 
@@ -17,21 +23,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-}
-
-//SEGUES
-- (IBAction)unwindToEvents:(UIStoryboardSegue *)sender {
+    
+    [self fetchEvents];
     
 }
 
-/*
+#pragma mark - Parse
+- (void)fetchEvents {
+    PFQuery *query = [PFQuery queryWithClassName:@"Event"];
+    [query orderByDescending:@"createdAt"];
+    query.limit = 20;
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *events, NSError *error) {
+        if (events != nil) {
+            self.events = (NSMutableArray *) events;
+            
+            [self.eventsTableView reloadData];
+            
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+    
+}
+
+#pragma mark - UITableViewDataSource
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    EventCell *cell = [self.eventsTableView dequeueReusableCellWithIdentifier:@"eventCell"];
+    
+    Event *event = self.events[indexPath.row];
+    [cell setEvent:event];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.events.count;
+}
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)unwindToEvents:(UIStoryboardSegue *)sender {
+    
 }
-*/
 
 @end
