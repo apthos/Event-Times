a23084acabd2761f3785e3b2d5dd3f9ea6e6fa78
apthos <david00lara@gmail.com>
2020-07-31 09:29:24 -0700

    Added profile information

--- Event Times/View Controllers/ProfileViewController.m
+++ Event Times/View Controllers/ProfileViewController.m
@@ -9,11 +9,16 @@
 #import "ProfileViewController.h"
 #import "SceneDelegate.h"
 #import "LoginViewController.h"
-#import <Parse/Parse.h>
+
+@import Parse;
 
 #pragma mark -
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet PFImageView *profileImageView;
+@property (strong, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *emailLabel;
 
 - (IBAction)onLogoutPress:(id)sender;
 
@@ -26,9 +31,96 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UITapGestureRecognizer *photoTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onPhotoTap:)];
+    [self.profileImageView addGestureRecognizer:photoTapRecognizer];
+    [self.profileImageView setUserInteractionEnabled:YES];
+    
+    [self displayUserDetails];
+    
+}
+
+#pragma mark - Utilities
+
+/**
+ */
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+#pragma mark - Setup
+
+- (void)displayUserDetails {
+    self.usernameLabel.text = PFUser.currentUser.username;
+    self.emailLabel.text = PFUser.currentUser.email;
+    
+    PFFileObject *imageFile = [PFUser.currentUser objectForKey:@"profileImage"];
+    
+    if (imageFile != nil) {
+        self.profileImageView.file = imageFile;
+        [self.profileImageView loadInBackground];
+    }
+    
+}
+
+#pragma mark - Parse
+
+/**
+ */
+- (void)saveProfileImage:(UIImage *)image {
+    UIImage *resizedPhoto = [self resizeImage:image withSize:CGSizeMake(1000.0, 1000.0)];
+    NSData *imageData = UIImagePNGRepresentation(resizedPhoto);
+    PFFileObject *imageFile = [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+    
+    self.profileImageView.file = imageFile;
+    [self.profileImageView loadInBackground];
+    
+    [PFUser.currentUser setObject:imageFile forKey:@"profileImage"];
+    [PFUser.currentUser saveInBackground];
+}
+
+#pragma mark - UIImagePickerControllerDelegate
+
+/**
+ */
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    [self saveProfileImage:editedImage];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
 }
 
 #pragma mark - Actions
+
+/**
+ */
+- (IBAction)onPhotoTap:(UITapGestureRecognizer *)tapGestureRecognizer {
+    UIImagePickerController *imagePickerController = [UIImagePickerController new];
+    imagePickerController.delegate = self;
+    imagePickerController.allowsEditing = YES;
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+    
+}
 
 /** User chose to logout by pressing the "Logout" button.
 
